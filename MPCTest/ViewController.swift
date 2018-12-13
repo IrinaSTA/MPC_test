676a72dd26f990dd1c43d948ad72263923cad20e
Irina Baldwin <2007inb@gmail.com>
2018-12-13 14:42:27 +0000

    Initial Commit

---
+++ MPCTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MPCTest
+//
+//  Created by Irina Baldwin on 13/12/2018.
+//  Copyright © 2018 Irina Baldwin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
